64d630ade43132021b51da23ea4c8472d264f44f
Attila Miklosi <attila.miklosi@gmail.com>
2017-12-13 16:36:42 +0100

    wip

--- src/ScheduleSession.js
+++ src/ScheduleSession.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import ScheduleSessionTable from "./ScheduleSessionTable"
+import moment from 'moment'
+import styles from './ScheduleSession.scss'
+
+const WeekSelector = ({goPrev, goNext}) => <div className={styles.weekSelector}>
+  <div onClick={goPrev} className={styles.prevCell}>&lt; Prev</div>
+  <div>b</div>
+  <div>c</div>
+  <div>d</div>
+  <div>e</div>
+  <div>f</div>
+  <div>g</div>
+  <div onClick={goNext} className={styles.nextCell}>Next &gt;</div>
+</div>
 
 export default class ScheduleSession extends React.Component {
   constructor() {
     super()
+    let day = moment()
+    day.startOf('isoWeek')
+
     this.state = {
       user1Availability: [
         [5, 6],
@@ -18,15 +34,29 @@ export default class ScheduleSession extends React.Component {
 
       bookedTimes: [
         9, 12, 45
-      ]
+      ],
+      startDate: day,
+      weekDelta: 0
     }
 
+    console.log(day.isoWeekday());
+    console.log(day.format('MM/D'));
+  }
+
+  nextWeek = () => {
+    this.setState({weekDelta: this.state.weekDelta + 1})
+  }
+
+  prevWeek = () => {
+    this.setState({weekDelta: this.state.weekDelta - 1})
   }
 
   render() {
     return <div>
       Schedule Session
+      <WeekSelector goPrev={this.prevWeek} goNext={this.nextWeek}/>
       <ScheduleSessionTable
+        startDate={this.state.startDate.clone().add(this.state.weekDelta, 'weeks')}
         user1Availability={this.state.user1Availability}
         user2Availability={this.state.user2Availability}
         bookedTimes={this.state.bookedTimes}
--- src/ScheduleSessionTable.js
+++ src/ScheduleSessionTable.js
@@ -1,40 +1,56 @@
 import React from 'react'
 import _ from 'lodash'
-import styles from './ScheduleSessionTable.scss'
+import styles from './ScheduleSession.scss'
 import {codeToTime, isAvailable, isBookedTime, mondayBasedDays} from './date-utils'
+import moment from 'moment'
+
 
 export default class ScheduleSessionTable extends React.Component {
+  handleMouseOver = (dayIdx, idx) => {
+    this.setState({selectedTimeCode: dayIdx * 24 + idx})
+  }
+
   getCellClass = (dayIdx, hourIdx) => {
     const cellCode = dayIdx * 24 + hourIdx
     const isUser1Available = isAvailable(cellCode, this.props.user1Availability)
     const isUser2Available = isAvailable(cellCode, this.props.user2Availability)
-    if (isBookedTime(cellCode, this.props.bookedTimes)) {
-      return styles.booked
-    }
-    if (isUser1Available && isUser2Available) {
-      return styles.bookable
-    }
-    if (isUser1Available) {
-      return styles.user1Available
-    }
-    if (isUser2Available) {
-      return styles.user2Available
-    }
+    const selectedTimeCode = _.get(this.state, 'selectedTimeCode')
+    const isSelected = cellCode == selectedTimeCode || cellCode == selectedTimeCode + 0.5
+    const style = (() => {
+      if (isBookedTime(cellCode, this.props.bookedTimes)) {
+        return styles.booked
+      }
+      if (isUser1Available && isUser2Available) {
+        return styles.bookable
+      }
+      if (isUser1Available) {
+        return styles.user1Available
+      }
+      if (isUser2Available) {
+        return styles.user2Available
+      }
+      return styles.none
+    })()
+    return style + ' ' + (isSelected ? styles.selected : '')
   }
 
   render() {
+    const startDate = moment(this.props.startDate)// || moment()
     const table = <table className={styles.table}>
       <thead>
       <tr>
         <th className={styles.cell}></th>
-        {mondayBasedDays.map(day => <th key={day} className={styles.cell}>{day}</th>)}
+        {_.range(7).map(day => <th key={day} className={styles.cell}>
+            {startDate.clone().add(day, 'days').format('MM/D')}<br/>
+            {startDate.clone().add(day, 'days').format('ddd')}
+          </th>)}
       </tr>
       </thead>
       <tbody>
       {_.range(48).map(idx =>
         <tr key={idx}>
           <td>{idx % 2 == 0 && codeToTime(idx / 2, 0)}</td>
-          {_.range(7).map(dayIdx => <td className={this.getCellClass(dayIdx, idx / 2)} key={dayIdx}>&nbsp;</td>)}
+          {_.range(7).map(dayIdx => <td className={this.getCellClass(dayIdx, idx / 2)} key={dayIdx} onMouseOver={this.handleMouseOver.bind(this, dayIdx, idx / 2)}>&nbsp;</td>)}
         </tr>)
       }
       </tbody>
